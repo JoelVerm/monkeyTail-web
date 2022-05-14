cd615983e2bda45cec2dd510dda6a2455ca9c475
Joël Vermeulen <joel.vermeulen5@outlook.com>
2022-05-14 19:08:03 +0200

    while loop works now! also with condition lambda

--- mt.js
+++ mt.js
@@ -122,7 +122,7 @@ const functions = {
 	or: c(async (a, b) => a || b, 'or', 'any', 'any'),
 	not: c(async a => !a, 'not', 'any'),
 	if: c(async (a, b, c) => (a ? b() : c()), 'if', 'any', 'block', 'block'),
-	while: c(async (a, b) => {while (a) a = b(a)}, 'while', 'any', 'block'),
+	while: c(async (a, b, c) => {while (await b(a)) a = await c(a); return a}, 'while', 'any', 'block', 'block'),
 	type: c(async a => getType(a), 'type', 'any'),
 	length: c(async a => a.length, 'length', 'any'),
 	print: c(
