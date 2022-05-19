b3cf4d8c3b925767c1f96ae38dfb76604c8c700c
Joël Vermeulen <joel.vermeulen5@outlook.com>
2022-05-19 18:58:11 +0200

    added dom.mt.js

---
+++ dom.mt.js
@@ -0,0 +1,17 @@
+Object.assign(functions, {
+	$get: c(
+		async name =>
+			name.startsWith('#')
+				? document.querySelector(name)
+				: document.querySelectorAll(name),
+		'$get',
+		'string'
+	),
+	on: c(
+		async (el, name, fn) => el.addEventListener(name, (el, ev) => fn(ev)),
+		'on',
+		'map',
+		'string',
+		'block'
+	)
+})
